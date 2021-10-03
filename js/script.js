9348365a3547efe595a10070e4c1a392271cdf37
Lou-biz18 <61588632+Lou-biz18@users.noreply.github.com>
2021-10-03 20:42:41 +0200

    meteo par ville day

--- js/script.js
+++ js/script.js
@@ -9,14 +9,15 @@ function start() {
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
-    .fetchTodayForecast()//tache lourde, recup toute les données : jouer avec ca 
+    //.fetchTodayForecast()//tache lourde, recup toute les données : jouer avec ca 
+    .fetchThreeDayForecast()
     .then(function(response) {
       // Récupère la donnée d'une API
       const data = response.data;
 
       // On récupère l'information principal
       const main = data.weather[0].main;
-      const description = data.weather[0].description;
+      const description = data.weather[1].description;
       const temp = data.main.temp;
       const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
 
